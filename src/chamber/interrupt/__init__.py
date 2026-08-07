8167a78c60d3b64bf761cd098acc35c560affec7
mavenrank <maverickodes.mac@gmail.com>
2026-08-07 15:34:43 +0530

    feat: interrupt package exports

---
+++ src/chamber/interrupt/__init__.py
@@ -0,0 +1,10 @@
+from chamber.interrupt.detect import Challenge, ChallengeKind, detect_challenge
+from chamber.interrupt.handoff import HandoffResult, hand_off
+
+__all__ = [
+    "Challenge",
+    "ChallengeKind",
+    "HandoffResult",
+    "detect_challenge",
+    "hand_off",
+]
