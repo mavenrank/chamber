e745bf5b3f8089addb0a397224abccfc57a8eb13
mavenrank <maverickodes.mac@gmail.com>
2026-08-07 15:34:20 +0530

    feat: browser package exports

---
+++ src/chamber/browser/__init__.py
@@ -0,0 +1,5 @@
+from chamber.browser.discovery import BrowserBuild, discover_browser, find_ubo
+from chamber.browser.launcher import launch
+from chamber.browser.profile import prepare_profile
+
+__all__ = ["BrowserBuild", "discover_browser", "find_ubo", "launch", "prepare_profile"]
